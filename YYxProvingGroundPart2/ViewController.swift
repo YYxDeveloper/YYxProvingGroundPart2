0450e5ba4a6cfbc4739200e429650b31f82eb34a
young <yyxdev@gmail.com>
2021-06-21 18:55:19 +0800

    Initial Commit

---
+++ YYxProvingGroundPart2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YYxProvingGroundPart2
+//
+//  Created by 呂子揚 on 2021/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
